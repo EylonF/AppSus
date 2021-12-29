a4c23ce3cca061582504d74eafce7387b7019288
eylon frisch <you@example.com>
2021-12-29 16:26:01 +0200

    links

---
+++ pages/NoteApp.jsx
@@ -0,0 +1,20 @@
+export class NoteApp extends React.Component {
+
+    state = {
+       
+    }
+
+    componentDidMount() {
+        console.log('NoteApp did mount')
+    }
+
+    render() {
+        return (
+            <section className="note-app">
+          <h1>Note App</h1>
+            </section>
+        )
+    }
+
+    
+}
