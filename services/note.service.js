99c697348a697919644dfc0457f2abf546e0e8fe
Oren-Papismedov <orenpapi11@gmail.com>
2021-12-29 19:29:41 +0200

    note serivice & note app

---
+++ services/note.service.js
@@ -0,0 +1,65 @@
+import { storageService } from './storage.service.js'
+
+export const noteService = {
+    query,
+}
+
+const STORAGE_KEY = 'noteDB'
+
+const gNotes = [
+    {
+     id: "n101",
+     type: "note-txt",
+     isPinned: true,
+     info: {
+     txt: "Fullstack Me Baby!"
+     }
+    },
+    {
+     id: "n102",
+     type: "note-img",
+     info: {
+     url: "http://some-img/me",
+     title: "Bobi and Me"
+     },
+     style: {
+     backgroundColor: "#00d"
+     }
+    },
+    {
+     id: "n103",
+     type: "note-todos",
+     info: {
+     label: "Get my stuff together",
+     todos: [
+     { txt: "Driving liscence", doneAt: null },
+     { txt: "Coding power", doneAt: 187111111 }
+     ]
+     }
+    }
+    ];
+
+
+    _createNotes()
+
+    function query(filterBy=null) {
+        const notes = _loadEmailsFromStorage()
+        if (!filterBy) return Promise.resolve(notes) 
+    }
+
+    function _createNotes(){
+        let Notes = _loadEmailsFromStorage()
+        if (!Notes || !Notes.length) {
+            Notes = gNotes
+        }
+        _saveEmailsToStorage(emails)
+    }
+
+    
+function _saveEmailsToStorage(emails) {
+    storageService.saveToStorage(STORAGE_KEY, emails)
+}
+
+function _loadEmailsFromStorage() {
+    return storageService.loadFromStorage(STORAGE_KEY)
+}
