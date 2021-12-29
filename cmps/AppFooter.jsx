075bafdf9287fcfab41728c5472d50271216ea40
eylon frisch <you@example.com>
2021-12-29 14:49:50 +0200

    sss

--- cmps/AppFooter.jsx
+++ cmps/AppFooter.jsx
@@ -6,8 +6,7 @@ export function AppFooter() {
         <section className="app-footer">
             <div className="footer-container main-layout">
 
-                <h1>APP<span>SUS</span></h1>
-                <img src="../assets/img/sus-logo.png" alt="" />
+                
                 <small>© coffe-rights 2021</small>
             </div>
         </section>
