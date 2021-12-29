85d3d1c3b92a19de2c6c954bd2f5364c10e5dcb6
Oren-Papismedov <orenpapi11@gmail.com>
2021-12-29 17:07:38 +0200

    main home h1 h2

--- pages/home.jsx
+++ pages/home.jsx
@@ -4,8 +4,15 @@ export class Home extends React.Component {
     render() {
         return (
             <section className="main-hero main-layout">
-                <h2>Welcome to</h2>   
-                <h1>APP<span>SUS</span></h1>
+                <h2><span className="letter1">W</span>
+                <span className="letter2">e</span>
+                <span className="letter3">l</span>
+                <span className="letter4">c</span>
+                <span className="letter5">0</span>
+                <span className="letter6">m</span>
+                <span className="letter7">e</span> <span className="letter8">t</span>
+                <span className="letter9">0</span></h2>   
+                <h1><span className="logoLetter1">A</span><span className="logoLetter2">P</span><span className="logoLetter3">P</span><span className="logo"><span className="logoLetter4">S</span><span className="logoLetter5">U</span><span className="logoLetter6">S</span></span></h1>
                 <img src="assets/img/sus-logo.png" alt="" />
                 <div className="btn-container">
                 <button className="btn-home1">Email App</button>
@@ -18,7 +25,4 @@ export class Home extends React.Component {
 
 
 
-{/* <span className="3"></span>
-<span className="4"></span>
-<span className="5"></span>
-<span className="6"></span> */}
+<span className="letter1"></span>
