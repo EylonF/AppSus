95dc7d935527ab13bb4040449b64f24b23d6727f
eylon frisch <you@example.com>
2021-12-30 21:23:26 +0200

    email app preview

--- cmps/EmailList.jsx
+++ cmps/EmailList.jsx
@@ -3,7 +3,7 @@ import {EmailPreview} from './EmailPreview.jsx'
 export function EmailList({emails}) {
     // console.log('email fro list',emails)
     return (
-        <section className="email-list">
+        <section className="email-list accordion accordion-flush">
             {emails.map(email => <EmailPreview email={email}/>)}
         </section>
     )
--- cmps/EmailPreview.jsx
+++ cmps/EmailPreview.jsx
@@ -1,17 +1,35 @@
-import {utilService} from '../services/util.service.js'
+import { utilService } from '../services/util.service.js'
 
 export function EmailPreview({ email }) {
     // console.log('email from preview', email)
     const { id, subject, body, isRead, sentAt, to } = email
 
     let formattedTime = utilService.getFormattedTime(sentAt)
-
+    const isReadClass = (isRead) ? 'email-read' : ''
     return (
-        <article className="email-preview" id={id}>
-            <p>&#9733;</p>
-            <p>{to}</p>
-            <p>{subject}</p>
-            <p>{formattedTime}</p>
+
+        // <div class={`accordion-item email-preview ${isReadClass}`}>
+        //     <h2 class="accordion-header" id="headingOne">
+        //         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        //             <p>&#9733;</p>
+        //             <p>{to}</p>
+        //             <p>{subject}</p>
+        //             <p>{formattedTime}</p>
+        //         </button>
+        //     </h2>
+        //     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+        //         <div class="accordion-body">
+        //             <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        //         </div>
+        //     </div>
+        // </div>
+
+
+        <article className={`email-preview ${isReadClass}`} id={id}>
+        <p>&#9733;</p>
+        <p>{to}</p>
+        <p>{subject}</p>
+        <p>{formattedTime}</p>
         </article>
     )
 }
