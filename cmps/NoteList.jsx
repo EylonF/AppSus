5e6c3a3e1fd85222cd12f13271d1cb806c0812ab
Oren-Papismedov <orenpapi11@gmail.com>
2022-01-01 17:40:22 +0200

    1

--- cmps/NoteList.jsx
+++ cmps/NoteList.jsx
@@ -1,10 +1,24 @@
 import { NotePreview } from './NotePreview.jsx'
+import { Masonry } from '../lib/react-masonry-css.js'
 
-export function NoteList({notes, onDeleteNote,loadNotes}) {
+export function NoteList({ notes, onDeleteNote, loadNotes }) {
     // console.log('note for list',notes)
     return (
-        <section className="note-list">
-            {notes.map(note => <NotePreview note={note} onDeleteNote={onDeleteNote} loadNotes={loadNotes}/>)}
-        </section>
+
+        // <Masonry
+        // breakpointCols={3}
+        // className="my-masonry-grid"
+        // columnClassName="my-masonry-grid_column"
+        
+        // >
+
+        <section>
+            <div className="note-list">
+                {notes.map(note => (
+                    <NotePreview note={note} onDeleteNote={onDeleteNote} loadNotes={loadNotes} />
+                    ))}
+                    </div>
+                    </section>
     )
+    {/* </Masonry> */}
 }
