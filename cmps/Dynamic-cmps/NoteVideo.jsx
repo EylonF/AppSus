e3a1e9b50d3f036bb23ad5faa87f440357273206
Oren-Papismedov <orenpapi11@gmail.com>
2022-01-01 10:08:26 +0200

    color save

--- cmps/Dynamic-cmps/NoteVideo.jsx
+++ cmps/Dynamic-cmps/NoteVideo.jsx
@@ -3,7 +3,7 @@
 export function NoteVideo({note, onDeleteNote, noteColor}) {   
 
     return (
-        <article className="card-note card text-white"style={{ backgroundColor: noteColor }} >
+        <article className="card-note"style={{ backgroundColor: noteColor }} >
             <h4 contenteditable="true">{note.info.title}</h4>
             <iframe width="400" height="295"
                 src={note.info.url}>
